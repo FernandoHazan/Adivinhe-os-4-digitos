dae5d624a65f9f544981b55d6c01b626dbfaeea3
fernando hazan vieira <fernandohvk4@gmail.com>
2024-05-27 14:27:43 -0300

    falta pouco

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 let numeroAleatorio = []
 let numeroChutado = []
+let posicao = 0
+let tem = 0
+
+const tentativas = document.createElement()
+const secundario = document.querySelector('.secundario')
+
+tentativas.classList.add("tentativas")
+tentativas.
+
+
 gerar()
 
 function gerar(){
 
-    while(numeroAleatorio.length < 4){
+    numeroAleatorio = []
+
+    while (numeroAleatorio.length < 4){
         
         temporario = parseInt(Math.random() * 10)
 
@@ -12,12 +24,43 @@ function gerar(){
             numeroAleatorio.push(temporario)
         }
     }
+    console.log(numeroAleatorio)
+
+    for(let u = 1; u < 5; u++){
+
+        document.getElementById('numero' + u).value = '' 
+       }
 }
 
 function chutar() {
     numeroChutado = []
+    posicao = 0
+    tem = 0
+
     for(let i = 1; i < 5; i++){
 
-        numeroChutado.push(document.getElementById('numero' + i).value)
+        numeroChutado.push(parseInt(document.getElementById('numero' + i).value))
     }
+
+
+    for(let e = 0; e < 4; e++){
+
+        if(parseInt(numeroChutado[e]) == numeroAleatorio[e]){
+            posicao++
+        }
+
+        if(numeroAleatorio.includes(numeroChutado[e])){
+            tem++
+        }
+    }
+
+    for(let u = 1; u < 5; u++){
+
+        document.getElementById('numero' + u).value = '' 
+       }
+
+       secundario.insertAdjacentElement("beforeend", tentativas)
 }
+
+
+
